5926f2880e3c2dcdb618f1fdc5fe09216f9d6e02
Miguel Angel Jimenez <macbookpro@miguelon.local>
2018-04-23 10:35:56 -0500

    Initial Commit

---
+++ StickyCorners/AppDelegate.swift
@@ -0,0 +1,12 @@
+/*    
+    Abstract:
+    Main application entry point.
+*/
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: Properties
+    
+    var window: UIWindow?
+}
